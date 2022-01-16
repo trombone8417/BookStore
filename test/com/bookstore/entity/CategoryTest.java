2afaf4b8504b6afb40a4b57a88bb4ced64484532
trombone8417 <trombone8417@yahoo.com>
2022-01-16 09:15:11 +0800

    Generate Code for Model Classes

--- test/com/bookstore/entity/CategoryTest.java
+++ test/com/bookstore/entity/CategoryTest.java
@@ -3,28 +3,25 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import com.bookstore.entity.Users;
+import com.bookstore.entity.Category;
 
-public class UsersTest {
+public class CategoryTest {
 
 	public static void main(String[] args) {
-		Users user1 = new Users();
-		user1.setEmail("you@gmail.com");
-		user1.setFullName("Mr President");
-		user1.setPassword("power");
+		Category newCat = new Category("Advanced Java");
 		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BookStore");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
 		entityManager.getTransaction().begin();
 		
-		entityManager.persist(user1);
+		entityManager.persist(newCat);
 		
 		entityManager.getTransaction().commit();
 		entityManager.clear();
 		entityManagerFactory.close();
 		
-		System.out.println("A Users object was persisted");
+		System.out.println("A Category object was persisted");
 
 	}
 
