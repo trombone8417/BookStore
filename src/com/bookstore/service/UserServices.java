e0f32a3a89589998776ce5dee5fb77997e62938c
trombone8417 <trombone8417@yahoo.com>
2022-01-22 19:21:35 +0800

    Implement List Users Feature v1

---
+++ src/com/bookstore/service/UserServices.java
@@ -0,0 +1,28 @@
+package com.bookstore.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.bookstore.dao.UserDAO;
+import com.bookstore.entity.Users;
+
+
+public class UserServices {
+	private EntityManagerFactory entityManagerFactory;
+	private EntityManager entityManager;
+	private UserDAO userDAO;
+	
+	public UserServices() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("BookStore");
+		entityManager = entityManagerFactory.createEntityManager();
+		userDAO = new UserDAO(entityManager);
+	}
+
+	public void listUser() {
+		List<Users> listUsers = userDAO.listAll();
+	}
+
+}
