e83a06c6731259b7b68ddab07e80c6c4f0a49522
HSU KUEI-CHING <32897048+trombone8417@users.noreply.github.com>
2022-03-13 22:20:54 +0800

    Calculate Average Rating of a Book

---
+++ test/com/bookstore/entity/BookRatingTest.java
@@ -0,0 +1,62 @@
+package com.bookstore.entity;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class BookRatingTest {
+
+	@Test
+	public void testAverageRating1() {
+		Book book = new Book();
+		
+		Set<Review> reviews = new HashSet<>();
+		Review review1 = new Review();
+		review1.setRating(5);
+		reviews.add(review1);
+		
+		book.setReviews(reviews);
+		
+		float averageRating = book.getAverageRating();
+		
+		assertEquals(5.0, averageRating, 0.0);
+	}
+	
+	@Test
+	public void testAverageRating2() {
+		Book book = new Book();
+		
+		float averageRating = book.getAverageRating();
+		
+		assertEquals(0.0, averageRating, 0.0);
+	}
+
+
+	@Test
+	public void testAverageRating3() {
+		Book book = new Book();
+		
+		Set<Review> reviews = new HashSet<>();
+		Review review1 = new Review();
+		review1.setRating(5);
+		reviews.add(review1);
+		
+		Review review2 = new Review();
+		review2.setRating(4);
+		reviews.add(review2);
+		
+		Review review3 = new Review();
+		review3.setRating(3);
+		reviews.add(review3);
+		
+		book.setReviews(reviews);
+		
+		float averageRating = book.getAverageRating();
+		
+		assertEquals(4.0, averageRating, 0.0);
+	}
+
+}
