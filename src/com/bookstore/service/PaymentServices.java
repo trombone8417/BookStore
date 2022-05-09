8c85e08df71095b828e79593779283adf73c1942
HSU KUEI-CHING <32897048+trombone8417@users.noreply.github.com>
2022-05-09 18:51:22 +0800

    Code PaymentServices class - Payer Info and Redirect URLs

--- src/com/bookstore/service/PaymentServices.java
+++ src/com/bookstore/service/PaymentServices.java
@@ -4,6 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bookstore.entity.BookOrder;
+import com.bookstore.entity.Customer;
+import com.paypal.api.payments.Payer;
+import com.paypal.api.payments.PayerInfo;
+import com.paypal.api.payments.RedirectUrls;
 
 public class PaymentServices {
 	private static final String CLIENT_ID = "";
@@ -21,13 +25,39 @@ public class PaymentServices {
 	
 	public void authorizePayment(BookOrder order) {
 		//get payer information
+		Payer payer = new Payer();
+		payer.setPaymentMethod("paypal");
+		
+		Customer customer = order.getCustomer();
+		
+		PayerInfo payerInfo = new PayerInfo();
+		payerInfo.setFirstName(customer.getFirstname());
+		payerInfo.setLastName(customer.getLastname());
+		payerInfo.setEmail(customer.getEmail());
+		payer.setPayerInfo(payerInfo);
 		
 		//get redirect URLs
+		String requestURL = request.getRequestURI().toString();
+		String requestURI = request.getRequestURI();
+		String baseURL = requestURL.replace(requestURI, "").concat(request.getContextPath());
+		
+		RedirectUrls redirectUrls = new RedirectUrls();
+		String cancelUrl = baseURL.concat("/view_cart");
+		String returnUrl = baseURL.concat("/review_payment");
+		
+		redirectUrls.setCancelUrl(cancelUrl);
+		redirectUrls.setReturnUrl(returnUrl);
 		
 		//get amount details
 		
 		//shipping address (recipient info)
 		
 		//get transaction details
+		
+		// request payment
+		
+		// get approval link
+		
+		// redirect to Paypal's payment page
 	}
 }
