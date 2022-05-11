e83898d0f626745251c55241fffa374e169fe2f2
HSU KUEI-CHING <32897048+trombone8417@users.noreply.github.com>
2022-05-11 20:09:06 +0800

    Code PaymentServices class - Shipping Address and Transaction Details

--- src/com/bookstore/service/PaymentServices.java
+++ src/com/bookstore/service/PaymentServices.java
@@ -1,16 +1,25 @@
 package com.bookstore.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bookstore.entity.Book;
 import com.bookstore.entity.BookOrder;
 import com.bookstore.entity.Customer;
+import com.bookstore.entity.OrderDetail;
 import com.paypal.api.payments.Amount;
 import com.paypal.api.payments.Details;
+import com.paypal.api.payments.Item;
+import com.paypal.api.payments.ItemList;
 import com.paypal.api.payments.Payer;
 import com.paypal.api.payments.PayerInfo;
 import com.paypal.api.payments.RedirectUrls;
+import com.paypal.api.payments.ShippingAddress;
+import com.paypal.api.payments.Transaction;
 
 public class PaymentServices {
 	private static final String CLIENT_ID = "";
@@ -40,6 +49,56 @@ public class PaymentServices {
 		// get approval link
 		
 		// redirect to Paypal's payment page
+	}
+	
+	private ShippingAddress getRecipientInformation(BookOrder order) {
+		ShippingAddress shippingAddress = new ShippingAddress();
+		String recipientName = order.getFirstname() + " " + order.getLastname();
+		shippingAddress.setRecipientName(recipientName)
+						.setPhone(order.getPhone())
+						.setLine1(order.getAddressLine1())
+						.setLine2(order.getAddressLine2())
+						.setCity(order.getCity())
+						.setState(order.getState())
+						.setCountryCode(order.getCountry())
+						.setPostalCode(order.getZipcode());
+		
+		return shippingAddress;
+	}
+	
+	private List<Transaction> getTransactionInformation(BookOrder order) {
+		Transaction transaction = new Transaction();
+		transaction.setDescription("Books ordered on Evergreen Books");
+		Amount amount = getAmountDetails(order);
+		transaction.setAmount(amount);
+		
+		ItemList itemList = new ItemList();
+		ShippingAddress shippingAddress = getRecipientInformation(order);
+		itemList.setShippingAddress(shippingAddress);
+		
+		List<Item> paypalItems = new ArrayList<>();
+		Iterator<OrderDetail> iterator = order.getOrderDetails().iterator();
+		
+		while (iterator.hasNext()) {
+			OrderDetail orderDetail = iterator.next();
+			Book book = orderDetail.getBook();
+			Integer quantity = orderDetail.getQuantity();
+			
+			Item paypalItem = new Item();
+			paypalItem.setCurrency("USD")
+						.setName(book.getTitle())
+						.setQuantity(String.valueOf(quantity))
+						.setPrice(String.valueOf(book.getPrice()));
+			paypalItems.add(paypalItem);
+		}
+		
+		itemList.setItems(paypalItems);
+		transaction.setItemList(itemList);
+		
+		List<Transaction> listTransation = new ArrayList<>();
+		listTransation.add(transaction);
+		
+		return listTransation;
 	}
 	
 	private Payer getPayerInformation(BookOrder order) {
